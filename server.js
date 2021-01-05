e1e3eaab98dfca55afc3aa76c0140ebd6723f558
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-05 11:27:35 +0000

    some changes - working

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express'); // import express from 'express'
 const app = express(); // get a handle on express
 
+const cors = require("cors");
+app.use(cors()); // use cors (security problem with using different ports for client and server)
+
 // request and response - you can use any name
 app.get("/", function(request, response) {
-    response.send('Hello Dunya');
+    response.send({message: "Hello Dunya"});
 });
-
 
 // Listen for requests
 app.listen(3000, function () {
