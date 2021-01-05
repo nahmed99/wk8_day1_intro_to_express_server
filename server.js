86e6dd0ec35a14bb05df15fd0464ee87df5ebe62
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-05 10:32:44 +0000

    Added listener to serve code

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express'); // import express from 'express'
-const app = express();
+const app = express(); // get a handle on express
 
 // request and response - you can use any name
 app.get("/", function(request, response) {
     response.send('Hello World');
+});
+
+
+// Listen for requests
+app.listen(3000, function () {
+    console.log("App running on port 3000");
 });
